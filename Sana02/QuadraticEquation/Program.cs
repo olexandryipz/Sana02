10b8eecfc9476b8b9acc0d99037b3e96de4c8ca8
ipz231_soyu <ipz231_soyu@student.ztu.edu.ua>
2023-11-21 21:11:53 +0200

    Change project QuadraticEquation

--- Sana02/QuadraticEquation/Program.cs
+++ Sana02/QuadraticEquation/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введіть коефіцієнти квадратного рівняння ax^2 + bx + c = 0:");
+
+        Console.Write("Введіть a: ");
+        double a = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Введіть b: ");
+        double b = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Введіть c: ");
+        double c = Convert.ToDouble(Console.ReadLine());
+
+        double discriminant = b * b - 4 * a * c;
+
+        if (discriminant > 0)
+        {
+            double x1 = (-b + Math.Sqrt(discriminant)) / (2 * a);
+            double x2 = (-b - Math.Sqrt(discriminant)) / (2 * a);
+
+            Console.WriteLine($"Дискримінант: {discriminant}");
+            Console.WriteLine($"Два розв'язки: x1 = {x1}, x2 = {x2}");
+        }
+        else if (discriminant == 0)
+        {
+            double x = -b / (2 * a);
+            Console.WriteLine($"Дискримінант: {discriminant}");
+            Console.WriteLine($"Один розв'язок: x = {x}");
+        }
+        else
+        {
+            Console.WriteLine("Розв'язків немає");
+        }
+    }
+}
