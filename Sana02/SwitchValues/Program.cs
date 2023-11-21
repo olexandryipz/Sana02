8e9cd2f14065f3dd908a9ef8c0a345332e732b22
ipz231_soyu <ipz231_soyu@student.ztu.edu.ua>
2023-11-21 21:21:57 +0200

    Change project SwitchValues

--- Sana02/SwitchValues/Program.cs
+++ Sana02/SwitchValues/Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введіть ціле число від 0 до 9: ");
+        int number;
+
+        bool isReadSuccess = int.TryParse(Console.ReadLine(), out number);
+
+        if (!isReadSuccess)
+        {
+            Console.WriteLine("Число має бути цілим");
+            return;
+        }
+        switch (number)
+        {
+            case 0:
+                Console.WriteLine("0 - нуль");
+                break;
+            case 1:
+                Console.WriteLine("1 - один");
+                break;
+            case 2:
+                Console.WriteLine("2 - два");
+                break;
+            case 3:
+                Console.WriteLine("3 - три");
+                break;
+            case 4:
+                Console.WriteLine("4 - чотири");
+                break;
+            case 5:
+                Console.WriteLine("5 - п'ять");
+                break;
+            case 6:
+                Console.WriteLine("6 - шість");
+                break;
+            case 7:
+                Console.WriteLine("7 - сім");
+                break;
+            case 8:
+                Console.WriteLine("8 - вісім");
+                break;
+            case 9:
+                Console.WriteLine("9 - дев'ять");
+                break;
+            default:
+                Console.WriteLine("Помилка. Потрібно ввести число від 0 до 9.");
+                break;
+        }
+    }
+}
